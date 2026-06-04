401f0b97cbedcea9bacd1be15ddf903dcecafa33
Benja <petrikor.ia@gmail.com>
2026-06-04 15:54:49 -0400

    fix twilio lazy init

--- services/twilio.js
+++ services/twilio.js
@@ -1,11 +1,9 @@
-const twilio = require('twilio');
-
-const client = twilio(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-);
-
 async function sendWhatsApp(to, message) {
+  const twilio = require('twilio');
+  const client = twilio(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN
+  );
   const toFormatted = to.startsWith('whatsapp:') ? to : `whatsapp:${to}`;
   return client.messages.create({
     from: process.env.TWILIO_WHATSAPP_FROM,
